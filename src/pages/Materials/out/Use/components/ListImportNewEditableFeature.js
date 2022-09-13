ae9a63437ad7a1e0eaacae7e662b018b76b358f3
Mykael Mello <mykael.mello@ufrn.br>
2022-09-13 06:31:54 -0300

    filtroID em andamento

--- src/pages/Materials/out/Use/components/ListImportNewEditableFeature.js
+++ src/pages/Materials/out/Use/components/ListImportNewEditableFeature.js
@@ -8,11 +8,97 @@ import { FaPlus } from 'react-icons/fa';
 import { Container, Row, Col, Card, Button, Form } from 'react-bootstrap';
 
 // import generic table from material's components with global filter and nested row
-import TableGfilterNestedrow from '../../../components/TableGfilterNestedRow';
+import TableGfilterNestedrowEditable from '../../../components/TableGfilterNestedRowEditable';
+
+// Create an editable cell renderer
+function EditableCell({
+  value: initialValue,
+  row: { index },
+  column: { id },
+  updateMyData, // This is a custom function that we supplied to our table instance
+  handlePushItem,
+}) {
+  // We need to keep and update the state of the cell normally
+  const [value, setValue] = React.useState(initialValue);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  // We'll only update the external data when the input is blurred
+  const onBlur = () => {
+    // não precisa atualizar o dado na tabela, só vou utilizar para importar para a lista de saída de material
+    // updateMyData(index, id, value);
+  };
+
+  // If the initialValue is changed external, sync it up with our state
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  // Check to make sure just quantity column is editable
+  if (id === 'quantity') {
+    return (
+      <Col className="d-flex">
+        <Form.Control
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          type="number"
+          id={`qs_${index}`}
+          size="sm"
+        />
+        <Button
+          onClick={(e) => handlePushItem(e, index)}
+          variant="outline-success"
+          size="sm"
+          className="border-0"
+        >
+          <FaPlus size={18} />
+        </Button>
+      </Col>
+    );
+  }
+  return value;
+}
 
 export default function Index(props) {
   const { push, items, materialsBalance } = props;
   const [materials, setMaterials] = useState([...materialsBalance]);
+  const [originalData] = useState(materials);
+  const [skipPageReset, setSkipPageReset] = React.useState(false);
+  // We need to keep the table from resetting the pageIndex when we
+  // Update data. So we can keep track of that flag with a ref.
+
+  // When our cell renderer calls updateMyData, we'll use
+  // the rowIndex, columnId and new value to update the
+  // original data
+  const updateMyData = (rowIndex, columnId, value) => {
+    // We also turn on the flag to not reset the page
+    setSkipPageReset(true);
+    setMaterials((old) =>
+      old.map((row, index) => {
+        if (index === rowIndex) {
+          return {
+            ...old[rowIndex],
+            [columnId]: value,
+          };
+        }
+        return row;
+      })
+    );
+  };
+
+  // After data chagnes, we turn the flag back off
+  // so that if data actually changes when we're not
+  // editing it, the page is reset
+  React.useEffect(() => {
+    setSkipPageReset(false);
+  }, [materials]);
+
+  // Let's add a data resetter/randomizer to help
+  // illustrate that flow...
+  const resetData = () => setMaterials(originalData);
 
   const handleQuantityChange = (e, row) => {
     const errors = [];
@@ -56,24 +142,11 @@ export default function Index(props) {
       quantity: row.values.quantity ?? 0,
     });
 
-    // remover da lista de pesquisa (atrapalha a filtragem, tem que remover e automaticamente aplicar a filtragem pra dar certo), usar um filtro pra esconder a linha adicionada, pode ser filtro de coluna com o id. Outra opção é construir uma lista dos itens que foram enviados e não permitir repetições. O filtro de coluna me parece a melhor opção, inclusve pode ocultar aqueles itens que ja vem da lista na outra pagina
-    // const materialsCopy = [...materials];
-    // materialsCopy.splice(row.index, 1);
-    // setMaterials(materialsCopy);
+    // remover da lista de pesquisa
+    const materialsCopy = [...materials];
+    materialsCopy.splice(row.index, 1);
+    setMaterials(materialsCopy);
   };
-
-  // Define a custom filter filter function!
-  function filterDifferentThan(rows, id, filterValue) {
-    return rows.filter((row) => {
-      const rowValue = row.values[id];
-      return rowValue !== '302400000066'; // example
-    });
-  }
-
-  // trigger to custom filter
-  function DefaultColumnFilter({ column: { filterValue, setFilter } }) {
-    return setFilter(filterValue); // Set undefined to remove the filter entirely
-  }
 
   const columns = React.useMemo(
     () => [
@@ -98,8 +171,6 @@ export default function Index(props) {
         width: 125,
         disableResizing: true,
         isVisible: window.innerWidth > 576,
-        Filter: DefaultColumnFilter,
-        filter: filterDifferentThan,
       },
       { Header: 'Denominação', accessor: 'name' },
       {
@@ -118,29 +189,13 @@ export default function Index(props) {
         // Make an expander cell
         Header: 'Saída', // No header
         id: 'quantity', // It needs an ID
+        accessor: 'quantity',
         width: 120,
         disableResizing: true,
-        Cell: ({ row }) => (
-          <Col className="d-flex">
-            <Form.Control
-              id={`s_${row.values.material_id}`}
-              size="sm"
-              type="number"
-              onChange={(e) => handleQuantityChange(e, row)}
-            />
-            <Button
-              onClick={(e) => handlePushItem(e, row)}
-              variant="outline-success"
-              size="sm"
-              className="border-0"
-            >
-              <FaPlus size={18} />
-            </Button>
-          </Col>
-        ),
+        // Cell: () => <span>{3}</span>,
       },
     ],
-    [materials]
+    []
   );
 
   const data = React.useMemo(() => materials, [materials]);
@@ -148,10 +203,11 @@ export default function Index(props) {
   const defaultColumn = React.useMemo(
     () => ({
       // Let's set up our default Filter UI
-      // Filter: DefaultColumnFilter, //coloca filtro para todas as colunas
+      // Filter: DefaultColumnFilter,
       minWidth: 30,
       width: 120,
       maxWidth: 800,
+      Cell: EditableCell,
     }),
     []
   );
@@ -215,13 +271,15 @@ export default function Index(props) {
         </Card.Text>
       </Row>
 
-      <TableGfilterNestedrow
+      <TableGfilterNestedrowEditable
         columns={columns}
         data={data}
         defaultColumn={defaultColumn}
         initialState={initialState}
         filterTypes={filterTypes}
         renderRowSubComponent={renderRowSubComponent}
+        updateMyData={updateMyData}
+        skipPageReset={skipPageReset}
       />
     </Container>
   );
