5299a56cf57658f5e7f7399b2005c929e97a692b
Mykael Mello <mykael.mello@ufrn.br>
2022-10-03 20:58:38 -0300

    up

--- src/pages/Materials/out/Loss/index.js
+++ src/pages/Materials/out/Loss/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
+import Select from 'react-select';
+import { colourOptions, groupedOptions } from './data';
+
+const groupStyles = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+};
+const groupBadgeStyles = {
+  backgroundColor: '#EBECF0',
+  borderRadius: '2em',
+  color: '#172B4D',
+  display: 'inline-block',
+  fontSize: 12,
+  fontWeight: 'normal',
+  lineHeight: '1',
+  minWidth: 1,
+  padding: '0.16666666666667em 0.5em',
+  textAlign: 'center',
+};
+
+const formatGroupLabel = (data) => (
+  <div style={groupStyles}>
+    <span>{data.label}</span>
+    <span style={groupBadgeStyles}>{data.options.length}</span>
+  </div>
+);
+
 export default function index() {
   return (
-    <div style={{ height: '600px', lineHeight: '600px', textAlign: 'center' }}>
-      Módulo de saída por extravio em desenvolvimento.
-    </div>
+    <Select
+      defaultValue={colourOptions[1]}
+      options={groupedOptions}
+      formatGroupLabel={formatGroupLabel}
+    />
   );
 }
