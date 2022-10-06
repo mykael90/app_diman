4ccaca60b10b577746ab6fbe37c1bf9d6f1685bb
Mykael Mello <mykael.mello@ufrn.br>
2022-10-06 07:33:17 -0300

    up

--- src/pages/Materials/internal/Restrict/index.js
+++ src/pages/Materials/internal/Restrict/index.js
@@ -1,9 +1,343 @@
-import React from 'react';
+/* eslint-disable no-param-reassign */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable no-nested-ternary */
+import React, { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 
-export default function index() {
+import { FaChartLine } from 'react-icons/fa';
+
+import {
+  Container,
+  Col,
+  Row,
+  Card,
+  Button,
+  Dropdown,
+  OverlayTrigger,
+  Tooltip,
+} from 'react-bootstrap';
+
+import axios from '../../../../services/axios';
+import Loading from '../../../../components/Loading';
+
+// import generic table from material's components with global filter and nested row
+import TableGfilterNestedrow from '../../components/TableGfilterNestedRow';
+import TableNestedrow from '../../components/TableNestedRow';
+
+export default function Index() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [reqs, setReqs] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsLoading(true);
+        const response = await axios.get('/materials/in/');
+        setReqs(response.data);
+        setIsLoading(false);
+      } catch (err) {
+        // eslint-disable-next-line no-unused-expressions
+        err.response?.data?.errors
+          ? err.response.data.errors.map((error) => toast.error(error)) // errors -> resposta de erro enviada do backend (precisa se conectar com o back)
+          : toast.error(err.message); // e.message -> erro formulado no front (é criado pelo front, não precisa de conexão)
+        setIsLoading(false);
+      }
+    }
+
+    getData();
+  }, []);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        // Make an expander cell
+        Header: () => null, // No header
+        id: 'expander', // It needs an ID
+        width: 30,
+        disableResizing: true,
+        Cell: ({ row }) => (
+          // Use Cell to render an expander for each row.
+          // We can use the getToggleRowExpandedProps prop-getter
+          // to build the expander.
+          <span {...row.getToggleRowExpandedProps()}>
+            {row.isExpanded ? '▽' : '▷'}
+          </span>
+        ),
+      },
+      {
+        Header: 'Nº RM',
+        accessor: 'req',
+        width: 120,
+        disableResizing: true,
+      },
+      {
+        Header: 'Req. Man.',
+        accessor: 'reqMaintenance',
+        width: 120,
+        disableResizing: true,
+      },
+      {
+        Header: 'Tipo',
+        accessor: 'type',
+        width: 120,
+        disableResizing: true,
+      },
+      {
+        Header: 'Valor',
+        accessor: 'value',
+        width: 120,
+        disableResizing: true,
+      },
+      {
+        Header: 'Pedido em:',
+        accessor: 'registerDate',
+        width: 120,
+        disableResizing: true,
+      },
+      {
+        Header: 'Pedido por:',
+        accessor: 'requiredBy',
+        width: 150,
+        disableResizing: true,
+        Cell: (props) => {
+          const custom = String(props.value).replace(
+            /(^[a-z]*)\.([a-z]*).*/gm,
+            '$1.$2'
+          ); // deixar só os dois primeiros nomes
+          return <span> {custom}</span>;
+        },
+      },
+      {
+        Header: 'Receb. em:',
+        accessor: 'createdAt',
+        width: 120,
+        disableResizing: true,
+      },
+      {
+        Header: 'Receb. por:',
+        accessor: 'receivedBy',
+        width: 150,
+        disableResizing: true,
+        Cell: (props) => {
+          const custom = String(props.value).replace(
+            /(^[a-z]*)\.([a-z]*).*/gm,
+            '$1.$2'
+          ); // deixar só os dois primeiros nomes
+          return <span> {custom}</span>;
+        },
+      },
+      {
+        Header: 'Unidade de Custo',
+        accessor: 'costUnit',
+        isVisible: window.innerWidth > 576,
+        // eslint-disable-next-line react/destructuring-assignment
+        Cell: (props) => {
+          const custom = String(props.value).replace(/([0-9]{2})/gm, '$1.');
+          return (
+            <span title={props.row.original.costUnitNome}>
+              {custom} {props.row.original.costUnitSigla}
+            </span>
+          );
+        },
+      },
+      {
+        Header: () => (
+          // FORMAT HEADER
+          <div className="p-auto text-center">Total</div>
+        ),
+        accessor: 'totalInventory',
+        width: 100,
+        disableResizing: true,
+        filterValue: 1,
+        disableSortBy: true,
+        filter: filterGreaterThan,
+        Filter: FilterForTotal,
+        Cell: ({ value, row }) => (
+          <div className="p-auto text-end">
+            {value}{' '}
+            <OverlayTrigger
+              placement="right"
+              delay={{ show: 250, hide: 400 }}
+              overlay={(props) =>
+                renderTooltip(props, 'Consultar histórico de transações')
+              }
+            >
+              <Button
+                size="sm"
+                variant="outline-primary"
+                className="border-0"
+                onClick={() => {
+                  handleShowModal(row.original.materialId);
+                }}
+              >
+                <FaChartLine />
+              </Button>
+            </OverlayTrigger>
+          </div>
+        ),
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => reqs, [reqs]);
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      // Filter: DefaultColumnFilter,
+      minWidth: 30,
+      width: 120,
+      maxWidth: 800,
+    }),
+    []
+  );
+
+  const initialState = {
+    sortBy: [
+      {
+        id: 'req',
+        desc: true,
+      },
+    ],
+    hiddenColumns: columns
+      .filter((col) => col.isVisible === false)
+      .map((col) => col.accessor),
+  };
+
+  const filterTypes = React.useMemo(
+    () => ({
+      // Override the default text filter to use
+      // "startWith"
+      text: (rows, ids, filterValue) => {
+        rows = rows.filter((row) =>
+          ids.some((id) => {
+            const rowValue = row.values[id];
+            const arrayFilter = String(filterValue).split(' ');
+
+            return arrayFilter.reduce((res, cur) => {
+              // res -> response; cur -> currency (atual)
+              res =
+                res &&
+                String(rowValue)
+                  .toLowerCase()
+                  .includes(String(cur).toLowerCase());
+              return res;
+            }, true);
+          })
+        );
+        return rows;
+      },
+    }),
+    []
+  );
+
+  const renderRowSubSubComponent = React.useCallback(
+    ({ row }) => (
+      <>
+        <span className="fw-bold">Especificação:</span>{' '}
+        {row.original.specification}
+      </>
+    ),
+    []
+  );
+
+  // Create a function that will render our row sub components
+  const renderRowSubComponent = React.useCallback(
+    ({ row }) => (
+      <TableNestedrow
+        style={{ padding: 0, margin: 0 }}
+        columns={[
+          {
+            // Make an expander cell
+            Header: () => null, // No header
+            id: 'expander', // It needs an ID
+            width: 30,
+            disableResizing: true,
+            Cell: ({ row }) => (
+              // Use Cell to render an expander for each row.
+              // We can use the getToggleRowExpandedProps prop-getter
+              // to build the expander.
+              <span {...row.getToggleRowExpandedProps()}>
+                {row.isExpanded ? '▽' : '▷'}
+              </span>
+            ),
+          },
+          {
+            Header: 'ID',
+            accessor: 'material_id',
+            width: 125,
+            disableResizing: true,
+            isVisible: window.innerWidth > 576,
+          },
+          { Header: 'Denominação', accessor: 'name' },
+          {
+            Header: 'Unidade',
+            accessor: 'unit',
+            width: 100,
+            disableResizing: true,
+          },
+          {
+            Header: 'Qtd',
+            accessor: 'quantity',
+            width: 100,
+            disableResizing: true,
+          },
+          {
+            Header: 'Valor',
+            accessor: 'value',
+            width: 100,
+            disableResizing: true,
+            // eslint-disable-next-line react/destructuring-assignment
+          },
+        ]}
+        data={row.original.MaterialInItems}
+        defaultColumn={{
+          // Let's set up our default Filter UI
+          // Filter: DefaultColumnFilter,
+          minWidth: 30,
+          width: 50,
+          maxWidth: 800,
+        }}
+        initialState={{
+          sortBy: [
+            {
+              id: 'name',
+              asc: true,
+            },
+          ],
+          hiddenColumns: columns
+            .filter((col) => col.isVisible === false)
+            .map((col) => col.accessor),
+        }}
+        filterTypes={filterTypes}
+        renderRowSubComponent={renderRowSubSubComponent}
+      />
+    ),
+    []
+  );
+
   return (
-    <div style={{ height: '600px', lineHeight: '600px', textAlign: 'center' }}>
-      Módulo de restrições em desenvolvimento
-    </div>
+    <>
+      <Loading isLoading={isLoading} />
+      <Container>
+        <Row className="text-center py-3">
+          <Card.Title>Relatório de Entrada: Materiais</Card.Title>
+          <Card.Text>
+            Materiais com entrada registrada por SIPAC, Doação, Retorno, etc.
+          </Card.Text>
+        </Row>
+
+        <TableGfilterNestedrow
+          columns={columns}
+          data={data}
+          defaultColumn={defaultColumn}
+          initialState={initialState}
+          filterTypes={filterTypes}
+          renderRowSubComponent={renderRowSubComponent}
+        />
+      </Container>
+    </>
   );
 }
