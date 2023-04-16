0c4458b5ad95364a3eec8026a957bcac721b2f82
Mykael Mello <eng.mykael@gmail.com>
2023-04-16 15:19:59 -0300

    styling recursive form

--- src/pages/Infra/Edificio/Add/index.js
+++ src/pages/Infra/Edificio/Add/index.js
@@ -1,101 +1,200 @@
-import React from 'react';
-import { Formik, Field, FieldArray } from 'formik';
-import { Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik';
+import * as Yup from 'yup';
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Badge,
+  OverlayTrigger,
+  Tooltip,
+  Form as BootstrapForm,
+} from 'react-bootstrap';
+import Select from 'react-select';
+import { FaPhone, FaPlus, FaTrashAlt } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
+import axios from '../../../../services/axios';
+import Loading from '../../../../components/Loading';
+import { primaryDarkColor } from '../../../../config/colors';
+
+const renderTooltip = (props, message) => (
+  <Tooltip id="button-tooltip" {...props}>
+    {message}
+  </Tooltip>
+);
+
+const emptyValues = {
+  sections: [],
+};
 
 // eslint-disable-next-line react/no-unstable-nested-components
 function Recursive({
   values,
   setFieldValue,
-  nameArray = 'friends',
+  nameArray = 'sections',
   level = 0,
 }) {
   return (
     <FieldArray name={nameArray}>
       {({ push, remove, swap }) => {
-        const addChild = (friend, index) => {
-          friend.friends.push({ name: '', email: '', friends: [] });
+        const addChild = (section, index) => {
+          section.sections.push({ name: '', email: '', sections: [] });
           // o método swap é só pra atualizar o estado pq nao utilizei funcao nativa do formik para adicionar o form filho
           swap(index, index);
         };
         return (
-          <div
-            className="border"
-            style={{ background: 'rgba(69, 98, 150, 0.25)' }}
-          >
-            {level === 0 ? (
-              <button
-                type="button"
-                onClick={() => push({ name: '', email: '', friends: [] })}
-                style={{ marginLeft: `${level * 20}px` }}
-              >
-                Add Friend
-              </button>
-            ) : null}
-
-            {values.friends.map((friend, index) => (
-              <div>
-                <div key={index}>
-                  <label
-                    htmlFor={`${nameArray}.${index}.name`}
+          <>
+            <Row className="pb-3">
+              {level === 0 ? (
+                <Col xs="auto">
+                  <Button
+                    variant="outline-primary"
+                    size="sm"
+                    onClick={() => push({ name: '', email: '', sections: [] })}
                     style={{ marginLeft: `${level * 20}px` }}
                   >
-                    Name
-                  </label>
-                  <Field
-                    id={`${nameArray}.${index}.name`}
-                    name={`${nameArray}.${index}.name`}
-                    type="text"
-                  />
-                  <label htmlFor={`${nameArray}.${index}.email`}>Email</label>
-                  <Field
-                    id={`${nameArray}.${index}.email`}
-                    name={`${nameArray}.${index}.email`}
-                    type="email"
-                  />
-                  <button type="button" onClick={() => remove(index)}>
-                    Remove Friend
-                  </button>
-                  <button type="button" onClick={() => addChild(friend, index)}>
-                    Add Child
-                  </button>
-                  {friend.friends.length > 0 ? (
-                    <>
-                      {/* <h4 style={{ marginLeft: `${level * 20}px` }}>Filhos</h4> */}
-                      <div>
+                    <FaPlus /> Divisão
+                  </Button>
+                </Col>
+              ) : null}
+            </Row>
+            <Row>
+              <Row>
+                {values.sections.map((section, index) => (
+                  <Row
+                    key={index}
+                    style={{ background: 'rgba(69, 98, 150, 0.25)' }}
+                  >
+                    <Row>
+                      <BootstrapForm.Group
+                        as={Col}
+                        xs={4}
+                        controlId={`${nameArray}.${index}.name`}
+                        // className="border-0 m-0 p-0 d-none"
+                      >
+                        <BootstrapForm.Label
+                        // htmlFor={`${nameArray}.${index}.name`}
+                        // style={{ marginLeft: `${level * 20}px` }}
+                        >
+                          Name
+                        </BootstrapForm.Label>
+                        <Field
+                          // id={`${nameArray}.${index}.name`}
+                          name={`${nameArray}.${index}.name`}
+                          type="text"
+                          as={BootstrapForm.Control}
+                          size="sm"
+                        />
+                      </BootstrapForm.Group>
+                      <BootstrapForm.Group
+                        as={Col}
+                        xs={4}
+                        controlId={`${nameArray}.${index}.email`}
+                        //  className="border-0 m-0 p-0 d-none"
+                      >
+                        <BootstrapForm.Label
+                        // htmlFor={`${nameArray}.${index}.email`}
+                        >
+                          Email
+                        </BootstrapForm.Label>
+                        <Field
+                          // id={`${nameArray}.${index}.email`}
+                          name={`${nameArray}.${index}.email`}
+                          type="email"
+                          as={BootstrapForm.Control}
+                          size="sm"
+                        />
+                      </BootstrapForm.Group>
+                      <Col xs={4}>
+                        <Button type="button" onClick={() => remove(index)}>
+                          Remove Section
+                        </Button>
+                        <Button
+                          type="button"
+                          onClick={() => addChild(section, index)}
+                        >
+                          Add Child
+                        </Button>
+                      </Col>
+                    </Row>
+                    <Row>
+                      {' '}
+                      {section.sections.length > 0 ? (
                         <Recursive
-                          values={friend}
+                          values={section}
                           setFieldValue={setFieldValue}
                           level={level + 1}
-                          nameArray={`${nameArray}.${index}.friends`}
+                          nameArray={`${nameArray}.${index}.sections`}
                         />
-                      </div>
-                    </>
-                  ) : null}
-                </div>
-              </div>
-            ))}
-          </div>
+                      ) : null}
+                    </Row>
+                  </Row>
+                ))}
+              </Row>
+            </Row>
+          </>
         );
       }}
     </FieldArray>
   );
 }
 
-function MyForm() {
-  const initialValues = {
-    friends: [],
-  };
+function MyForm({ initialValues = null }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isEditMode = !!initialValues;
 
   return (
-    <Formik initialValues={initialValues}>
-      {({ values, setFieldValue }) => (
-        <form>
-          <h3>Friends</h3>
-          <Recursive values={values} setFieldValue={setFieldValue} />
-          <button type="submit">Submit</button>
-        </form>
-      )}
-    </Formik>
+    <>
+      <Loading isLoading={isLoading} />
+      <Container>
+        <Row className="justify-content-center">
+          <Col
+            xs={12}
+            className=" text-center"
+            style={{ background: primaryDarkColor, color: 'white' }}
+          >
+            <span className="fs-5">
+              {isEditMode ? 'Editar' : 'Adicionar'} Divisões em Instalações
+              Físicas
+            </span>
+          </Col>
+        </Row>
+        <Row className="pt-2">
+          <Formik initialValues={emptyValues}>
+            {({
+              values,
+              errors,
+              touched,
+              setFieldValue,
+              handleReset,
+              handleChange,
+              handleBlur,
+              setFieldTouched,
+            }) => (
+              <Form as BootstrapForm onReset={handleReset}>
+                <h3>PRÉDIO TAL</h3>
+                <Recursive values={values} setFieldValue={setFieldValue} />
+                <Row className="justify-content-center pt-2 pb-4">
+                  <Col xs="auto" className="text-center">
+                    <Button variant="danger" type="reset">
+                      Limpar
+                    </Button>
+                  </Col>
+                  <Col xs="auto" className="text-center">
+                    <Button variant="success" type="submit">
+                      {isEditMode ? 'Alterar' : 'Cadastrar'}
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+            )}
+          </Formik>
+        </Row>
+      </Container>
+    </>
   );
 }
 
