81596a23cc70e2ca518bf4a402bed44d14f0223b
Mykael Mello <eng.mykael@gmail.com>
2023-06-04 21:03:55 -0300

    ok

--- src/pages/Infra/Edificio/Geolocation/index.js
+++ src/pages/Infra/Edificio/Geolocation/index.js
@@ -26,7 +26,7 @@ const onLoadMarker = (marker) => {
   console.log('marker: ', marker);
 };
 
-function MyComponent() {
+function MyComponent({ buildingData }) {
   const mapRef = useRef();
 
   const center = useMemo(
@@ -50,35 +50,79 @@ function MyComponent() {
   const onLoad = useCallback((map) => (mapRef.current = map), []);
 
   return (
-    <Container style={{ height: '70vh' }}>
-      <LoadScript
-        googleMapsApiKey={process.env.REACT_APP_BASE_GOOGLE_API_KEY}
-        region="us"
-        libraries={['geometry']}
-      >
-        <GoogleMap
-          // mapContainerStyle={containerStyle}
-          center={center}
-          zoom={15}
-          mapContainerClassName="map-container"
-          options={options}
-          onLoad={onLoad}
-          onDblClick={(e) => console.log(e)}
-        >
-          {/* Child components, such as markers, info windows, etc. */}
-          <Marker
-            onLoad={onLoadMarker}
-            position={position}
-            options={{
-              label: {
-                text: 'Posição teste',
-                className: 'mb-4 pb-3',
-              },
-            }}
-          />
-        </GoogleMap>
-      </LoadScript>
-    </Container>
+    <>
+      <Container className="border">
+        <Row>
+          <Col xs="auto" className="pe-0">
+            SUB-RIP:{' '}
+          </Col>
+          <Col>
+            <span className="fw-bold ps-0">{buildingData?.subRip}</span>{' '}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="auto" className="pe-0">
+            N INFRA:{' '}
+          </Col>
+          <Col>
+            <span className="fw-bold ps-0">{buildingData?.numInfra}</span>{' '}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="auto" className="pe-0">
+            INSTALAÇÃO:{' '}
+          </Col>
+          <Col>
+            <span className="fw-bold ps-0">{buildingData?.name}</span>{' '}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="auto" className="pe-0">
+            ÁREA:{' '}
+          </Col>
+          <Col xs="auto">
+            <span className="fw-bold ps-0">{buildingData?.area}</span> m²
+          </Col>
+          <Col xs="auto" className="pe-0">
+            PAVIMENTOS:{' '}
+          </Col>
+          <Col xs="auto">
+            <span className="fw-bold ps-0">{buildingData?.floors}</span>{' '}
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row style={{ height: '70vh' }}>
+          <LoadScript
+            googleMapsApiKey={process.env.REACT_APP_BASE_GOOGLE_API_KEY}
+            region="us"
+            libraries={['geometry']}
+          >
+            <GoogleMap
+              // mapContainerStyle={containerStyle}
+              center={center}
+              zoom={18}
+              mapContainerClassName="map-container"
+              options={options}
+              onLoad={onLoad}
+              onDblClick={(e) => console.log(e)}
+            >
+              {/* Child components, such as markers, info windows, etc. */}
+              <Marker
+                onLoad={onLoadMarker}
+                position={position}
+                options={{
+                  label: {
+                    text: 'Posição teste',
+                    className: 'mb-4 pb-3',
+                  },
+                }}
+              />
+            </GoogleMap>
+          </LoadScript>
+        </Row>
+      </Container>
+    </>
   );
 }
 
